80d2cc8c2f8410de276c8d1765a1153e9c8a0651
idoubi <me@idoubi.cc>
2023-02-14 12:45:06 +0800

    example for stream response

--- examples/completion_test.go
+++ examples/completion_test.go
@@ -3,6 +3,9 @@ package examples
 import (
 	"fmt"
 	"log"
+	"strings"
+
+	"github.com/tidwall/gjson"
 )
 
 func ExampleCreateCompletion() {
@@ -11,20 +14,28 @@ func ExampleCreateCompletion() {
 	uri := "/v1/completions"
 	params := map[string]interface{}{
 		"model":       "text-davinci-003",
-		"prompt":      "say hello three times",
+		"prompt":      "say hello to me 10 times",
 		"max_tokens":  2048,
 		"temperature": 0.9,
 		"n":           1,
-		"stream":      false,
+		"stream":      true,
 	}
 
 	res, err := cli.Post(uri, params)
-
 	if err != nil {
 		log.Fatalf("request api failed: %v", err)
 	}
 
-	fmt.Println(res.GetString("choices.0.text"))
+	msgs := []string{}
 
+	for data := range res.Stream() {
+		d := gjson.ParseBytes(data)
+		s := d.Get("choices.0.text").String()
+		msgs = append(msgs, s)
+
+		log.Printf("%s\n", s)
+	}
+
+	fmt.Printf("message is: %s", strings.Join(msgs, ""))
 	// Output: xxx
 }
--- go.mod
+++ go.mod
@@ -3,7 +3,6 @@ module github.com/chatgp/gpt3
 go 1.16
 
 require (
-	github.com/idoubi/goutils v1.3.0
-	github.com/idoubi/goz v1.4.2 // indirect
-	github.com/tidwall/gjson v1.14.4 // indirect
+	github.com/idoubi/goutils v1.3.2
+	github.com/tidwall/gjson v1.14.4
 )
